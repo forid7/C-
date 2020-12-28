5cf98b8a748d3855e3f40f5782a298cf0900e48b
forid7 <61509809+forid7@users.noreply.github.com>
2020-12-28 12:08:30 +0600

    Add files via upload

---
+++ MusicPlayer.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MusicPlayer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        String[] paths, files;
+        int mouseX = 0, mouseY = 0;
+        bool mouseDown;
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            mouseX = MousePosition.X - 200;
+            mouseY = MousePosition.Y - 40;
+
+            this.SetDesktopLocation(mouseX, mouseY);
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+        }
+
+        private void listBoxSong_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = paths[listBoxSong.SelectedIndex];
+        }
+
+        private void AddMusicButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = true;
+
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                files = ofd.SafeFileNames;
+                paths = ofd.FileNames;
+
+                for(int i=0;i<files.Length;i++)
+                {
+                    listBoxSong.Items.Add(files[i]);
+                }
+            }
+        }
+    }
+}
